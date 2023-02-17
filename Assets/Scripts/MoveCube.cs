51b5041ffcd7070c8ae141aa140c073b17a05888
Hunter <huntersfred@gmail.com>
2023-02-16 20:32:37 -0500

    Initial commit

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+
+    [SerializeField] private float distance = 10;
+    [SerializeField] private float speed;
+
+    private Rigidbody rb;
+
+    private void Awake() => rb = GetComponent<Rigidbody>();
+
+    private void FixedUpdate()
+    {
+        // Calculate the percentage based on the since of the time elapsed
+        var percentage = Mathf.Sin(Time.time / distance * Mathf.PI * 2);
+
+        // Clamp the percentage to be between 0 and 1
+        percentage = Mathf.Clamp01(percentage);
+
+        speed = percentage < .5f ? Mathf.Abs(speed) : -Mathf.Abs(speed);
+        print(percentage);
+        rb.velocity = new Vector3(speed, 0 , 0);
+
+    }
+}
